6fa8af27b165f671d31b14256441e670955aa3f6
chaweb <46398015+chaweb@users.noreply.github.com>
2022-08-11 18:16:12 +0200

    change nuxt-start to express for work with plesk

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const { Nuxt } = require('nuxt-start')
+const express = require('express');
+const { Nuxt, Builder } = require('nuxt');
 
-// Require nuxt config
-const config = require('./nuxt.config.js')
+const config = require('./nuxt.config.js');
 
-// Create a new nuxt instance (config needs dev: false)
-const nuxt = new Nuxt(config)
+// Create new express app
+const app = express();
 
-// Start nuxt server
-nuxt.listen(3000) // nuxt.listen(port, host)
+// Listen to port 3000 or PORT env if provided
+app.listen(process.env.PORT || 3000);
+
+// Enable production mode
+config.dev = false;
+
+// Create instance of nuxt
+const nuxt = new Nuxt(config);
+
+// Add nuxt middleware
+app.use(nuxt.render);
+
+// Build on start
+new Builder(nuxt).build().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
-export default {
+
+const pkg = require('./package')
+
+module.exports = {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'web-incroyaux',
